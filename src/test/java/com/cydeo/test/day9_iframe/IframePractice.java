23b391fcba2ff4414cac6bb16cb91defa28222ee
KennethB824 <kenneth.bermudez001@mymdc.net>
2022-06-08 20:16:53 -0400

    first commit

---
+++ src/test/java/com/cydeo/test/day9_iframe/IframePractice.java
@@ -0,0 +1,13 @@
+package com.cydeo.test.day9_iframe;
+
+import com.cydeo.test.base.TestBase;
+import org.testng.annotations.Test;
+
+public class IframePractice extends TestBase {
+
+    @Test
+    public void iframeTest(){
+
+    }
+
+}
